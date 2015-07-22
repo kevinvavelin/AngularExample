b503f75894da6cb732f7027a80fa4b9626a492c8
Vavelin Kevin <vavelinkevin@gmail.com>
2015-07-22 21:51:48 +0400

    Chat angular

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module("Chat", ["firebase"]);
+      var app = angular.module("Chat", ["firebase"]);
+
+      app.controller("ChatController", ["$scope", "$firebaseArray",
+        function($scope, $firebaseArray) {
+
+          var ref = new Firebase("VOTRE_ADRESSE_FIREBASE");
+
+          $scope.messages = $firebaseArray(ref);
+
+          $scope.addMessage = function(e) {
+
+            if (e.keyCode === 13 && $scope.msg) {
+
+              var name = $scope.name || "Anonyme";
+
+              $scope.messages.$add({
+                from: name,
+                body: $scope.msg
+              });
+
+              $scope.msg = "";
+            }
+          }
+        }
+      ]);
